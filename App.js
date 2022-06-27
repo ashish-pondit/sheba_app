d49399507ec7da4c1d1f46f8a6a56084c6f5e036
Ashish Pondit <ashishpondit@gmail.com>
2022-06-28 00:16:39 +0600

    initial cleaning

--- App.js
+++ App.js
@@ -10,47 +10,11 @@ import {
   View,
 } from 'react-native';
 
-// import {
-//   Colors,
-//   DebugInstructions,
-//   Header,
-//   LearnMoreLinks,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
-
-// const Section = ({children, title}): Node => {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// };
-
 const App: () => Node = () => {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <Text>Hello world</Text>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.Container}>
+      <Text>Hello world !!</Text>
+    </View>
   );
 };
 
